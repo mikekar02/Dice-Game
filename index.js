795104e2bd96409d1fb280a443f95ff278ac6842
Mike Karvelis <mikekar02@gmail.com>
2025-04-07 18:45:59 +0300

    added both dice with rng

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-randomNumber1 = Math.floor(Math.random() * 7);
-randomNumber2 = Math.floor(Math.random() * 7);
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;//rng
+var diceImage = "dice" + randomNumber1 + ".png";//dicex.png
+var diceImagePath = "./assets/" + diceImage;//./assets/dicex.png
+
+image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", diceImagePath);
+
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;//rng
+var diceImage = "dice" + randomNumber2 + ".png";//dicex.png
+var diceImagePath = "./assets/" + diceImage;//./assets/dicex.png
+
+image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", diceImagePath);
+
+if (randomNumber1 === randomNumber2){
+    
+}
